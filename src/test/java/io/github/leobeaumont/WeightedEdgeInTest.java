11d60cbe5aae2a42296fd34d7b01dc459d205eb6
BEAUMONT Léo <leowhite3005@gmail.com>
2025-10-25 16:00:31 +0200

    Added tests for WeightedEdgeIn class

---
+++ src/test/java/io/github/leobeaumont/WeightedEdgeInTest.java
@@ -0,0 +1,73 @@
+package io.github.leobeaumont;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the {@link WeightedEdgeIn} class.
+ */
+public class WeightedEdgeInTest {
+
+    private WeightedEdgeIn edge;
+
+    /**
+     * Tests the constructors of {@link WeightedEdgeIn}.
+     * <p>
+     * Verifies that:
+     * <ul>
+     *     <li>The constructor with only a weight initializes {@code origin} and {@code arrival} to {@code null}.</li>
+     *     <li>The constructor with {@link Place}, {@link Transition}, and weight correctly assigns the given arguments.</li>
+     * </ul>
+     * </p>
+     */
+    @Test
+    void constructorTest() {
+        // Tests uninitialized constructor
+        edge = new WeightedEdgeIn(2);
+        assertNull(edge.getOrigin());
+        assertNull(edge.getArrival());
+
+        // Tests initialized constructor
+        Place place = new Place();
+        Transition transition = new Transition();
+        edge = new WeightedEdgeIn(place, transition, 2);
+        assertSame(edge.getOrigin(), place);
+        assertSame(edge.getArrival(), transition);
+    }
+
+    /**
+     * Tests the {@link WeightedEdgeIn#isActivable()} method.
+     * <p>
+     * Verifies that an edge is activable only when its origin {@link Place}
+     * contains at least as many tokens as its weight.
+     * </p>
+     */
+    @Test
+    void isActivableTest() {
+        Place place = new Place();
+        edge = new WeightedEdgeIn(2);
+        edge.setOrigin(place);
+
+        assertFalse(edge.isActivable());
+        place.setNbTokens(2);
+        assertTrue(edge.isActivable());
+    }
+
+    /**
+     * Tests the {@link WeightedEdgeIn#activate()} method.
+     * <p>
+     * Verifies that activating a {@code WeightedEdgeIn} removes
+     * a number of tokens equal to its weight from its origin {@link Place}.
+     * </p>
+     */
+    @Test
+    void activateTest() {
+        Place place = new Place(3);
+        edge = new WeightedEdgeIn(2);
+        edge.setOrigin(place);
+
+        edge.activate();
+        assertEquals(place.getNbTokens(), 1);
+    }
+}
