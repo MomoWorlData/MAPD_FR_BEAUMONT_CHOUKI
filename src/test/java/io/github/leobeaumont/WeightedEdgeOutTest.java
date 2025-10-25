4c7e76c4e023096d62adac163e979086fabaadd1
BEAUMONT Léo <leowhite3005@gmail.com>
2025-10-25 16:06:36 +0200

    Added tests for WeightedEdgeOut class

---
+++ src/test/java/io/github/leobeaumont/WeightedEdgeOutTest.java
@@ -0,0 +1,55 @@
+package io.github.leobeaumont;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the {@link WeightedEdgeOut} class.
+ */
+public class WeightedEdgeOutTest {
+
+    private WeightedEdgeOut edge;
+
+    /**
+     * Tests the constructors of {@link WeightedEdgeOut}.
+     * <p>
+     * Verifies that:
+     * <ul>
+     *     <li>The constructor with only a weight initializes {@code origin} and {@code arrival} to {@code null}.</li>
+     *     <li>The constructor with {@link Transition}, {@link Place}, and weight correctly assigns the given arguments.</li>
+     * </ul>
+     * </p>
+     */
+    @Test
+    void constructorTest() {
+        // Tests uninitialized constructor
+        edge = new WeightedEdgeOut(2);
+        assertNull(edge.getOrigin());
+        assertNull(edge.getArrival());
+
+        // Tests initialized constructor
+        Place place = new Place();
+        Transition transition = new Transition();
+        edge = new WeightedEdgeOut(transition, place, 2);
+        assertSame(edge.getOrigin(), transition);
+        assertSame(edge.getArrival(), place);
+    }
+
+    /**
+     * Tests the {@link WeightedEdgeOut#activate()} method.
+     * <p>
+     * Verifies that activating a {@code WeightedEdgeOut} adds
+     * a number of tokens equal to its weight to its arrival {@link Place}.
+     * </p>
+     */
+    @Test
+    void activateTest() {
+        Place place = new Place();
+        edge = new WeightedEdgeOut(2);
+        edge.setArrival(place);
+
+        edge.activate();
+        assertEquals(place.getNbTokens(), 2);
+    }
+}
